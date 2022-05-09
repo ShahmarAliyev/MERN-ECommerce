08c0953a0131ed844c2ccb9f2c32532b8d3343f0
ShahmarALiyev <saliyev2019@gmail.com>
2022-05-08 20:15:31 -0500

    dpeploy fix

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-// const dotenv = require("dotenv");
 const cors = require("cors");
 const userRouter = require("./src/routes/user-router");
 const cartRouter = require("./src/routes/cart-router");
@@ -10,27 +9,36 @@ const orderRouter = require("./src/routes/order-router");
 const stripeRouter = require("./src/routes/stripe-router");
 const productsRouter = require("./src/routes/product-router");
 
-require("dotenv").config({ path: ".env" });
+const dotenv = require("dotenv");
 
+dotenv.config();
 const connectDB = async () => {
   try {
-    mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    mongoose.connect(
+      "mongodb+srv://mern:test12345@ecommerce.m34kr.mongodb.net/shop?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     console.log("Successfully connected to Mongo DB");
   } catch (error) {
     console.log(error);
   }
 };
 connectDB();
+app.use(express.json());
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method token, Access-Control-Request-Headers"
+  );
   next();
 });
-app.use(express.json());
-app.use(cors());
-
 app.use("/api/users", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/products", productsRouter);
